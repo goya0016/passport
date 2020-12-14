b0cc96b8ce0cb3911fe135cfc1782fddf8d33d56
Nipun goyal <goya0016@algonquinlive.com>
2020-12-14 16:28:20 -0500

    Initial Commit

---
+++ goya0016-FinalProjectTests/goya0016_FinalProjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  goya0016_FinalProjectTests.swift
+//  goya0016-FinalProjectTests
+//
+//  Created by Nipun Goyal on 2020-12-14.
+//
+
+import XCTest
+@testable import goya0016_FinalProject
+
+class goya0016_FinalProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
